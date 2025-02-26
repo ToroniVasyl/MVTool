92c398ffdb8056290abde7cff376320656115a79
kabzd <kabzdayt@gmail.com>
2025-02-26 15:18:20 +0200

    first config

---
+++ src/main/java/com/Main/Main.java
@@ -0,0 +1,31 @@
+package com.Main;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/main.fxml"));
+            Pane root = loader.load();
+
+            MainController controller = loader.getController();
+
+            Scene scene = new Scene(root, 1080, 720);
+            primaryStage.setTitle("MVTool");
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
